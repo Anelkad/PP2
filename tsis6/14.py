b2eed1f8ffaadd5d286993f681c4fb12f9dc6a14
Anel <an_kadyrova@kbtu.kz>
2021-03-02 18:49:51 +0600

    upd

---
+++ tsis6/14.py
@@ -0,0 +1,3 @@
+print(set('abcdefghijklmnopqrstuvwxyz')<=set(input().lower()))
+#a = set(input()).lower()
+#print(sum([i in a for i in 'abcdefghijklmnopqrstuvwxyz'])==26)
---
+++ tsis6/15.py
@@ -0,0 +1,2 @@
+print('-'.join(sorted([i for i in input().split('-')])))
+#print('-'.join(sorted(input().split('-'))))
