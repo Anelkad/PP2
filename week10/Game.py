98a226d242891e355e89521dc39985af52a0dd6b
Anel <an_kadyrova@kbtu.kz>
2021-04-11 10:54:19 +0600

    upd

--- week10/Game.py
+++ week10/Game.py
@@ -13,16 +13,33 @@ start = sc_width//5
 screen = pygame.display.set_mode((sc_width,sc_height))
 road_w = sc_width*2//3
 road_h = sc_height
-table = pygame.Surface((90,45))
+table = pygame.Surface((95,60))
 
 
 font = pygame.font.SysFont("Verdana", 60)
 game_over = font.render("Game Over", True, (255,0,0))
 
 score = 0
-score1 = 0 
-
+score1 = 0
+level = 1
+case = 0
 pygame.display.set_caption("Game")
+
+class rrect():
+    def __init__(self, y = 0, width=30 , height = 100, speed = 18):
+        self.width = width
+        self.height = height
+        self.speed = speed
+        self.y = y
+        self.x = start +road_w//2 - self.width/2
+
+    def draw(self):
+        pygame.draw.rect(screen,(255,255,255),pygame.Rect(self.x,self.y,self.width,self.height))
+
+    def fall(self):
+        self.y+=self.speed
+        if self.y > sc_height:
+            self.y = -self.height - 50
 
 class Enemy:
     def __init__(self,  width=40, height=80):
@@ -30,7 +47,7 @@ class Enemy:
         self.height = height
         self.x = random.randint(start,road_w + start - self.width)
         self.y = 0
-        self.speed = random.randint(18+score1//5,40+score1//5*(score+1))
+        self.speed = random.randint(18+score1//5,40+score1//5*(case))
         self.hitbox = pygame.Rect(self.x, self.y, self.width, self.height)
 
     def draw(self):
@@ -41,6 +58,7 @@ class Enemy:
                 pygame.image.load('bang.png'), (self.width*2, self.height)), (self.x-self.width, self.y))
 
     def fall(self):
+        global level
         global score
         self.y += self.speed
         if self.hitbox.colliderect(car.hitbox):
@@ -50,11 +68,12 @@ class Enemy:
             score +=1
             self.y = - self.height
             self.x = random.randint(start,road_w + start - self.width)
-            self.speed = random.randint(18+score1//5,40+score1//5*(score+1))
+            self.speed = random.randint(18+score1//5,40+score1//5*(case))
         elif self.y > sc_height + self.height:
+            level +=1
             self.y = - self.height
             self.x = random.randint(start,road_w + start - self.width)
-            self.speed = random.randint(18+score1//5,40+score1//5*(score+1))
+            self.speed = random.randint(18+score1//5,40+score1//5*(case))
         self.hitbox = pygame.Rect(self.x, self.y, self.width, self.height)
        
 
@@ -84,6 +103,10 @@ class Car:
 
 car = Car()
 evilcar = Enemy()
+obstacles=[]
+roadrect=[]
+for i in range(5):
+    roadrect.append(rrect(-100+150*i))
 
 class Coin():
     def __init__(self, x=10, y=10,  width=30, height=30, speed=18, dx=5):
@@ -125,9 +148,8 @@ def spawner():
 def live(score):
     for i in range(3-score):
         table.blit(pygame.transform.scale(
-            pygame.image.load('heart.png'), (20, 20)), (21*i, 20))
+            pygame.image.load('heart.png'), (20, 20)), (21*i, 39))
 
-obstacles=[]
 
 gameover = False
 
@@ -137,11 +159,14 @@ while not gameover:
     spawner()
     screen.blit(pygame.transform.scale(
         pygame.image.load('flower.png'), (sc_width, sc_height)),(0,0))
-    screen.blit(pygame.transform.scale(
-        pygame.image.load('road.png'), (road_w, road_h)),(start,0))
+    pygame.draw.rect(screen,(156,148,148),pygame.Rect(start,0,road_w,road_h))
     
     screen.blit(table,(5,5))
     table.fill((255,255,255))
+    
+    for r in roadrect:
+        r.draw()
+        r.fall()
     
     car.draw()
     car.move()
@@ -156,12 +181,26 @@ while not gameover:
     font = pygame.font.SysFont('ComicSans', 30)
     text = font.render(f'Score:{score1}', 1, (0, 0, 0))
     table.blit(text, (0, 0))
+    text1 = font.render(f'Level:{case}', 1, (0, 0, 0))
+    table.blit(text1, (0, 20))
     
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             gameover = True
 
     live(score)
+
+    if level//7 == case :
+        case+=1
+        s = pygame.Surface((sc_width,sc_height))
+        s.set_alpha(128)
+        s.fill((0,0,0))
+        screen.blit(s, (0,0))
+        font = pygame.font.SysFont('ComicSans', 40)
+        screen.blit(font.render(f'Level:{case}', 1, (255, 255, 255)), (sc_width/2-40,sc_height/2))
+        pygame.display.update()
+        time.sleep(2)
+
     if score >= 3:
         gameover = True
         s = pygame.Surface((sc_width,sc_height))
