05963ba169bf2595bf4d6a3f4ba4c9434828cb50
Anel <an_kadyrova@kbtu.kz>
2021-04-19 17:11:08 +0600

    upd

--- week11/1.py
+++ week11/1.py
@@ -6,7 +6,6 @@ import os
 
 pygame.init()
 
-score = 0
 level = 1
 
 clock = pygame.time.Clock()
@@ -14,12 +13,9 @@ sc_width = 600
 sc_height = 400
 sc = pygame.display.set_mode((sc_width,sc_height))
 table = pygame.Surface((90,30))
-FPS = 10
+FPS = 8
 
 pygame.display.set_caption("Snake game")
-
-gameover = False
-prev = False
 font = pygame.font.SysFont('ComicSans', 30)
 
 class Circle():
@@ -62,6 +58,10 @@ class Snakehead():
         self.x = 15
         self.y = 15
         self.speed = 10
+        self.up = False
+        self.down = False
+        self.right = False
+        self.left = False
         self.dx = self.speed
         self.dy = 0
         self.hitbox = pygame.Rect(self.x-self.r,self.y-self.r,self.r*2,self.r*2)
@@ -74,25 +74,39 @@ class Snakehead():
             for i in range(len(tail)-1):
                 tail[i]=Circle(tail[i+1].x,tail[i+1].y)
             tail[len(tail)-1]=Circle(self.x,self.y)
-        if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_LEFT:
-                self.dx = -self.speed
-                self.dy = 0
-            if event.key == pygame.K_RIGHT:
-                self.dx = self.speed
-                self.dy = 0
-            if event.key == pygame.K_UP:
-                self.dx = 0
-                self.dy = -self.speed
-            if event.key == pygame.K_DOWN:
-                self.dx = 0
-                self.dy = self.speed
+
+        keys = pygame.key.get_pressed()    
+        if keys[pygame.K_RIGHT] and not self.left:
+            self.right = True
+            self.up = self.down = self.left = False
+        elif keys[pygame.K_LEFT] and not self.right:
+            self.left = True
+            self.up = self.down = self.right = False
+        elif keys[pygame.K_UP] and not self.down:
+            self.up = True
+            self.down = self.right = self.left = False
+        elif keys[pygame.K_DOWN] and not self.up:
+            self.down = True
+            self.up = self.right = self.left = False
+
+        if self.right:
+            self.dx = self.speed
+            self.dy = 0
+        elif self.left:
+            self.dx = -self.speed
+            self.dy = 0
+        elif self.up:
+            self.dx = 0
+            self.dy = -self.speed
+        elif self.down:
+            self.dx = 0
+            self.dy = self.speed
+        
+        
         self.y+=self.dy
         self.x+=self.dx
         self.hitbox = pygame.Rect(self.x-self.r,self.y-self.r,self.r*2,self.r*2)
-        
 
-head = Snakehead()
 
 class Fruit():
     def __init__(self):
@@ -104,112 +118,280 @@ class Fruit():
     def draw(self):
         pygame.draw.circle(sc,(200,10,10),(self.x,self.y),self.r)
 
-f = Fruit()
+eviltail = []        
+class evilSnake(Snakehead):
+    def __init__(self):
+        Snakehead.__init__(self)
+        self.r = 5
+        self.x = sc_width-15
+        self.y = sc_height-15
+        self.dx = 0
 
-while not gameover:
-    while not prev:
-        s = pygame.Surface((sc_width,sc_height))
-        s.set_alpha(128)
-        s.fill((0,0,200))
-        sc.blit(s, (0,0))
-        sc.blit(font.render("CONTINUE GAME?", 1, (255, 0, 0)), (sc_width//3,sc_height/2))
-        sc.blit(font.render("y-Yes / n-No ", 1, (0, 0, 0)), (sc_width//3,sc_height/2+50))
-        pygame.display.update()
+    def move(self):
+        if len(eviltail)>0:
+            for i in range(len(eviltail)-1):
+                eviltail[i]=Circle(eviltail[i+1].x,eviltail[i+1].y)
+            eviltail[len(eviltail)-1]=Circle(self.x,self.y)
+
+        keys = pygame.key.get_pressed()    
+        if keys[pygame.K_d] and not self.left:
+            self.right = True
+            self.up = self.down = self.left = False
+        elif keys[pygame.K_a] and not self.right:
+            self.left = True
+            self.up = self.down = self.right = False
+        elif keys[pygame.K_w] and not self.down:
+            self.up = True
+            self.down = self.right = self.left = False
+        elif keys[pygame.K_s] and not self.up:
+            self.down = True
+            self.up = self.right = self.left = False
+
+        if self.right:
+            self.dx = self.speed
+            self.dy = 0
+        elif self.left:
+            self.dx = -self.speed
+            self.dy = 0
+        elif self.up:
+            self.dx = 0
+            self.dy = -self.speed
+        elif self.down:
+            self.dx = 0
+            self.dy = self.speed
+        
+        
+        self.y+=self.dy
+        self.x+=self.dx
+        self.hitbox = pygame.Rect(self.x-self.r,self.y-self.r,self.r*2,self.r*2)
+
+def supergame():
+    head = Snakehead()
+    evil = evilSnake()
+    global tail, eviltail
+    tail = []
+    f = Fruit()
+    
+    score1 = 0
+    score2 = 0
+
+    gameover1 = False
+    gameover2 = False
+    gameclose = False
+    while not gameclose:
+        sc.fill((0,0,0))
+        sc.blit(table,(sc_width-95,5))
+        table.fill((255,255,255))
+        table.blit(font.render("score1 "+str(score1), 1, (0, 0, 0)), (0,0))
+        table.blit(font.render("score2 "+str(score2), 1, (0, 0, 0)), (0,14))
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
-                prev=True
-                pygame.quit()
-            if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_y:
-                    pickle_out = open("f.pickle","rb")
-                    ex = pickle.load(pickle_out)
-                    score = ex["score"]
-                    level = ex["level"]
-                    FPS = ex["fps"]
-                    tail = [Circle(head.x,head.y) for i in range(ex["len"])]
-                    prev=True
-                if event.key ==pygame.K_n:
-                    prev=True
+                gameclose = True
 
-    sc.fill((0,0,0))
-    create_map(level)
-    sc.blit(table,(sc_width-95,5))
-    table.fill((255,255,255))
-    table.blit(font.render("score "+str(score), 1, (0, 0, 0)), (0,0))
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            gameover = True
-            pygame.quit()
-            
-    head.move()
-    if head.y-5<0 or head.y+5>sc_height or head.x-5<0 or head.x+5>sc_width:
-        gameover=True
-    head.draw()
+        head.move()
+        evil.move()
+        if head.y-5<0 or head.y+5>sc_height or head.x-5<0 or head.x+5>sc_width:
+            gameover1=True
+            gameclose=True
+        if evil.y-5<0 or evil.y+5>sc_height or evil.x-5<0 or evil.x+5>sc_width:
+            gameover2=True
+            gameclose=True
+        head.draw()
+        evil.draw()
 
-    for w in walls:
-        w.draw()
-        if head.hitbox.colliderect(w.hitbox):
-            gameover = True
-        if f.hitbox.colliderect(w.hitbox):
+        for c in tail:
+            c.draw()
+            if head.hitbox.colliderect(c.hitbox):
+                gameover1 = True
+                gameclose= True
+            if evil.hitbox.colliderect(c.hitbox):
+                gameover2 = True
+                gameclose= True
+        for c in eviltail:
+            c.draw()
+            if evil.hitbox.colliderect(c.hitbox):
+                gameover2 = True
+                gameclose= True
+            if head.hitbox.colliderect(c.hitbox):
+                gameover1 = True
+                gameclose= True
+        
+        f.draw()
+        if head.hitbox.colliderect(f.hitbox):
+            score1+=1
+            pygame.mixer.music.load('bonus.wav')
+            pygame.mixer.music.play(1)
             f = Fruit()
-    for c in tail:
-        c.draw()
-        if head.hitbox.colliderect(c.hitbox):
-            gameover = True
-    
-    f.draw()
-    if head.hitbox.colliderect(f.hitbox):
-        score+=1
-        pygame.mixer.music.load('bonus.wav')
-        pygame.mixer.music.play(1)
-        f = Fruit()
-        tail.append(Circle(head.x,head.y))
+            tail.append(Circle(head.x,head.y))
 
-    if score//5 ==level:
-        FPS+=1
-        level+=1
-        walls = []
-        tail = []
-        head = Snakehead()
-        if level >=6:
+        if evil.hitbox.colliderect(f.hitbox):
+            score2+=1
+            pygame.mixer.music.load('bonus.wav')
+            pygame.mixer.music.play(1)
+            f = Fruit()
+            eviltail.append(Circle(evil.x,evil.y))
+        clock.tick(8)
+        pygame.display.update()
+    if gameover1 or gameover2:
+        s = pygame.Surface((sc_width,sc_height))
+        s.set_alpha(128)
+        s.fill((200,0,0))
+        sc.blit(s, (0,0))
+        if gameover1: text = font.render("2nd Winner!", True, (0,0,0))
+        else: text = font.render("1st Winner!", True, (0,0,0))
+        sc.blit(text, (sc_width//4,sc_height/2-70))
+        sc.blit(font.render("press F to play again", 1, (255, 255, 255)), (sc_width/2-30,sc_height/2+40))
+        for event in pygame.event.get():
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_f:
+                    supergame()
+        pygame.display.update()
+        time.sleep(7)
+
+
+def main():
+    global tail, walls, FPS, level
+    score = 0
+    f = Fruit()
+    tail=[]
+    head = Snakehead()
+    gameover = False
+    gameclose = False
+    prev = False
+    while not gameclose:
+        while not prev:
             s = pygame.Surface((sc_width,sc_height))
             s.set_alpha(128)
             s.fill((0,0,200))
             sc.blit(s, (0,0))
-            sc.blit(font.render("WINNER!", 1, (255, 255, 255)), (sc_width/2-40,sc_height/2))
+            sc.blit(font.render("CONTINUE GAME?", 1, (255, 0, 0)), (sc_width//3,sc_height/2))
+            sc.blit(font.render("Yes: press y / No: press n", 1, (0, 0, 0)), (sc_width//3,sc_height/2+50))
             pygame.display.update()
-            time.sleep(2)
-            pygame.quit()
-        else:
-            s = pygame.Surface((sc_width,sc_height))
-            s.set_alpha(128)
-            s.fill((0,0,200))
-            sc.blit(s, (0,0))
-            sc.blit(font.render("LEVEL "+str(level), 1, (255, 255, 255)), (sc_width/2-40,sc_height/2))
-            pygame.display.update()
-            time.sleep(2)
-    
-    clock.tick(FPS)
-    pygame.display.update()
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    prev=True
+                    gameclose = True
+                if event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_y:
+                        pickle_out = open("f.pickle","rb")
+                        ex = pickle.load(pickle_out)
+                        score = ex["score"]
+                        level = ex["level"]
+                        FPS = ex["fps"]
+                        f = ex["fruit"]
+                        tail = [Circle(head.x,head.y) for i in range(ex["len"])]
+                        prev=True
+                    if event.key ==pygame.K_n:
+                        prev=True
 
-if gameover:
+        sc.fill((0,0,0))
+        create_map(level)
+        sc.blit(table,(sc_width-95,5))
+        table.fill((255,255,255))
+        table.blit(font.render("score "+str(score), 1, (0, 0, 0)), (0,0))
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                gameclose = True
+                
+        head.move()
+        if head.y-5<0 or head.y+5>sc_height or head.x-5<0 or head.x+5>sc_width:
+            gameover=True
+            gameclose=True
+        head.draw()
+
+        for w in walls:
+            w.draw()
+            if head.hitbox.colliderect(w.hitbox):
+                gameover = True
+                gameclose= True
+            if f.hitbox.colliderect(w.hitbox):
+                f = Fruit()
+        for c in tail:
+            c.draw()
+            if head.hitbox.colliderect(c.hitbox):
+                gameover = True
+                gameclose= True
+        
+        f.draw()
+        if head.hitbox.colliderect(f.hitbox):
+            score+=1
+            pygame.mixer.music.load('bonus.wav')
+            pygame.mixer.music.play(1)
+            f = Fruit()
+            tail.append(Circle(head.x,head.y))
+
+        if score//10 ==level:
+            FPS+=1
+            level+=1
+            walls = []
+            tail = []
+            head = Snakehead()
+            if level >=6:
+                s = pygame.Surface((sc_width,sc_height))
+                s.set_alpha(128)
+                s.fill((0,0,200))
+                sc.blit(s, (0,0))
+                sc.blit(font.render("WINNER!", 1, (255, 255, 255)), (sc_width/2-40,sc_height/2))
+                pygame.display.update()
+                time.sleep(2)
+                return
+            else:
+                s = pygame.Surface((sc_width,sc_height))
+                s.set_alpha(128)
+                s.fill((0,0,200))
+                sc.blit(s, (0,0))
+                sc.blit(font.render("LEVEL "+str(level), 1, (255, 255, 255)), (sc_width/2-40,sc_height/2))
+                pygame.display.update()
+                time.sleep(2)
+        
+        clock.tick(FPS)
+        pygame.display.update()
+
+    if gameover:
+        s = pygame.Surface((sc_width,sc_height))
+        s.set_alpha(128)
+        s.fill((200,0,0))
+        sc.blit(s, (0,0))
+        sc.blit(font.render("GAME OVER", True, (0,0,0)), (sc_width//4,sc_height/2-70))
+        sc.blit(font.render("Score : "+str(score), 1, (255, 255, 255)), (sc_width/2-30,sc_height/2))
+        sc.blit(font.render("press F to play again", 1, (255, 255, 255)), (sc_width/2-30,sc_height/2+40))
+        pygame.display.update()
+        time.sleep(7)
+        if level<6:
+            dic = {
+            "score" : score,
+            "level" : level,
+            "fps" : FPS,
+            "len" : len(tail),
+            "fruit" : f
+            }
+            pickle_out = open("f.pickle","wb")
+            pickle.dump(dic,pickle_out)
+            pickle_out.close()
+        for event in pygame.event.get():
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_f:
+                    main()
+
+    
+
+run = False
+while not run:
     s = pygame.Surface((sc_width,sc_height))
     s.set_alpha(128)
-    s.fill((200,0,0))
+    s.fill((0,0,200))
     sc.blit(s, (0,0))
-    sc.blit(font.render("Game Over", True, (0,0,0)), (sc_width//4,sc_height/2-70))
-    sc.blit(font.render("Score : "+str(score), 1, (255, 255, 255)), (sc_width/2-30,sc_height/2))
+    sc.blit(font.render("Game for 2?", 1, (255, 0, 0)), (sc_width//3,sc_height/2))
+    sc.blit(font.render("Yes: press y / No: press n", 1, (0, 0, 0)), (sc_width//3,sc_height/2+50))
     pygame.display.update()
-    time.sleep(3)
-
-if level<6:
-    dic = {
-    "score" : score,
-    "level" : level,
-    "fps" : FPS,
-    "len" : len(tail)
-    }
-    pickle_out = open("f.pickle","wb")
-    pickle.dump(dic,pickle_out)
-    pickle_out.close()
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            run=True
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_y:
+                run=True
+                supergame()
+            if event.key ==pygame.K_n:
+                run=True
+                main()
 
