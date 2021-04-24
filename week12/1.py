9a3bc4810c5dad2bf5a3748e94c63e69fd7945ee
Anel <an_kadyrova@kbtu.kz>
2021-04-24 12:47:00 +0600

    upd

--- week12/1.py
+++ week12/1.py
@@ -6,47 +6,54 @@ conn = psycopg2.connect(
     password="16anel16")
 cur = conn.cursor()
 
-#cur.execute(""" 
-#CREATE TABLE students (
-#  id      VARCHAR(10),
-#  name    VARCHAR(255),
-#  faculty VARCHAR(255),
-#  gpa     NUMERIC
-#);
-#""")
+def createtable():
+    cur.execute(""" 
+    CREATE TABLE students (
+    id      VARCHAR(10),
+    name    VARCHAR(255),
+    faculty VARCHAR(255),
+    gpa     NUMERIC
+    );
+    """)
 
-#sql = """
-#INSERT INTO students VALUES ('1','A','FIT',3.0),
-#('2','B','FIT',2.5),
-#('3','C','BS',4.0);"""
-#cur.execute(sql)
+def inserting():
+    sql = """
+    INSERT INTO students VALUES
+    ('4','D','BS',1.5),
+    ('5','E','FIT',2.0);
+    """
+    cur.execute(sql)
 
-#sql = """
-#UPDATE students SET id=%s WHERE name=%s
-#"""
-#name = 'A'
-#new_id = '4'
-#cur.execute(sql, ( new_id,name))
-#print(cur.rowcount)
-
-#sql = """
-#SELECT * FROM students
-#"""
-#cur.execute(sql)
-
-#row = cur.fetchone()
-
-#while row is not None:
-#    print(row)
-#    row = cur.fetchone()
-
-sql = """
-DELETE FROM students WHERE gpa<3.0
-"""
-cur.execute(sql)
-
-print(cur.rowcount)
+def updating():
+    sql = """
+    UPDATE students SET id=%s WHERE name=%s
+    """
+    name = 'A'
+    new_id = '4'
+    cur.execute(sql, (new_id,name))
+    print(cur.rowcount)
 
 
+def querying():
+    sql = """
+    SELECT * FROM students WHERE gpa>3.00
+    """
+    cur.execute(sql)
+
+    row = cur.fetchone()
+
+    while row is not None:
+        print(row)
+        row = cur.fetchone()
+
+def deleting(cur):
+    sql = """
+    DELETE FROM students WHERE gpa<2.0
+    """
+    cur.execute(sql)
+
+    print(cur.rowcount)
+
+querying()
 cur.close()
 conn.commit()
